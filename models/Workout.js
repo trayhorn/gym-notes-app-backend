e416a26b212a8d433a42d7e2e1f0e433bef63d6f
trayhorn <amoshyiivan@gmail.com>
2025-12-06 05:27:30 +0200

    changed supersetGroup data type

--- models/Workout.js
+++ models/Workout.js
@@ -5,7 +5,7 @@ const exerciseSchema = new Schema(
 		name: { type: String, required: true },
 		reps: { type: String, required: true },
 		weight: { type: String, required: true },
-		supersetGroup: { type: Number, required: false },
+		supersetGroup: { type: String, required: false },
 	},
 	{ _id: false }
 );
